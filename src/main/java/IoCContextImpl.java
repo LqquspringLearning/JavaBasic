13ab549dfbee1ea5958b6cccabf743c803a63cd8
Lianqing/Bluto <lqqu@thoughtworks.com>
2018-09-14 15:40:40 +0800

    simple ac for problems 2

--- src/main/java/IoCContextImpl.java
+++ src/main/java/IoCContextImpl.java
@@ -5,6 +5,8 @@ import java.util.Set;
 public class IoCContextImpl implements IoCContext {
 
     Set<Class<?>> containerSet = new HashSet<>();
+    Object locker = new Object();
+    public boolean available = true;
 
     @Override
     public void registerBean(Class<?> beanClazz) {
@@ -13,15 +15,26 @@ public class IoCContextImpl implements IoCContext {
 
     @Override
     public <T> T getBean(Class<T> resolveClazz) {
-        if (resolveClazz == null || !containerSet.contains(resolveClazz))
-            throw new IllegalArgumentException("resolveClazz is mandatory");
 
-        try {
-            return resolveClazz.newInstance();
-        } catch (InstantiationException e) {
-            throw new IllegalArgumentException(String.format("%s is abstract", resolveClazz.getName()));
-        } catch (IllegalAccessException e) {
-            throw new IllegalStateException(String.format("%s has no default constructor", resolveClazz.getName()));
+        if (!available) throw new IllegalStateException();
+
+        synchronized (locker) {
+            available = false;
+            if (resolveClazz == null)
+                throw new IllegalArgumentException("resolveClazz is mandatory");
+            if (!containerSet.contains(resolveClazz))
+                throw new IllegalStateException("resolveClazz is mandatory");
+            try {
+                return resolveClazz.newInstance();
+            } catch (InstantiationException e) {
+                throw new IllegalArgumentException(String.format("%s is abstract", resolveClazz.getName()));
+            } catch (IllegalAccessException e) {
+                throw new IllegalStateException(String.format("%s has no default constructor", resolveClazz.getName()));
+            }
+            finally {
+                available = true;
+            }
         }
+
     }
 }
--- src/test/java/IoCContextImplTest.java
+++ src/test/java/IoCContextImplTest.java
@@ -1,19 +1,15 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class IoCContextImplTest {
-    IoCContext context;
 
-    @BeforeEach
-    void setUp() {
-        context = new IoCContextImpl();
-    }
 
     @Test
     void should_create_a_instance() {
-
+        IoCContext context = new IoCContextImpl();
         context.registerBean(MyBean.class);
         MyBean myBeanInstance = context.getBean(MyBean.class);
         assertEquals(MyBean.class, myBeanInstance.getClass());
@@ -21,58 +17,90 @@ class IoCContextImplTest {
 
     @Test
     void should_throw_exception_when_class_null() {
+        IoCContext context = new IoCContextImpl();
         context.registerBean(MyBean.class);
         try {
             context.getBean(null);
-        } catch(IllegalArgumentException e){
-            assertSame(IllegalArgumentException.class,e.getClass());
-           assertEquals("resolveClazz is mandatory",e.getMessage());
+        } catch (IllegalArgumentException e) {
+            assertSame(IllegalArgumentException.class, e.getClass());
+            assertEquals("resolveClazz is mandatory", e.getMessage());
         }
 
     }
 
     @Test
     void should_throw_when_class_not_register() {
+        IoCContext context = new IoCContextImpl();
         try {
             context.getBean(MyBean.class);
-        } catch(IllegalArgumentException e){
-            assertSame(IllegalArgumentException.class,e.getClass());
-            assertEquals("resolveClazz is mandatory",e.getMessage());
+        } catch (IllegalStateException e) {
+            assertSame(IllegalStateException.class, e.getClass());
         }
 
     }
 
     @Test
-    void should_throw_excetion_when_class_is_abstract() {
+    void should_throw_exception_when_class_is_abstract() {
+        IoCContext context = new IoCContextImpl();
         context.registerBean(MyBeanAbstractClass.class);
-      try {
-          context.getBean(MyBeanAbstractClass.class);
-      } catch(IllegalArgumentException e){
-          assertSame(IllegalArgumentException.class, e.getClass());
-          assertEquals("MyBeanAbstractClass is abstract",e.getMessage());
-      }
+        try {
+            context.getBean(MyBeanAbstractClass.class);
+        } catch (IllegalArgumentException e) {
+            assertSame(IllegalArgumentException.class, e.getClass());
+            assertEquals("MyBeanAbstractClass is abstract", e.getMessage());
+        }
     }
 
     @Test
-    void should_throw_exception_when_calss_need_param_when_constract() {
-     context.registerBean(MyBeanAnother.class);
-     try {
-         context.getBean(MyBeanAnother.class);
-     } catch(IllegalArgumentException e){
-         assertSame(IllegalArgumentException.class, e.getClass());
-         assertEquals("MyBeanAnother is abstract",e.getMessage());
-     }
+    void should_throw_exception_when_register_type_is_interface() {
+       IoCContext context = new IoCContextImpl();
+       context.registerBean(IoCContext.class);
+        try {
+            context.getBean(IoCContext.class);
+        } catch (IllegalArgumentException e) {
+            assertSame(IllegalArgumentException.class, e.getClass());
+            assertEquals("IoCContext is abstract", e.getMessage());
+        }
+    }
+
+    @Test
+    void should_throw_exception_when_class_need_param_when_constract() {
+        IoCContext context = new IoCContextImpl();
+        context.registerBean(MyBeanAnother.class);
+        try {
+            context.getBean(MyBeanAnother.class);
+        } catch (IllegalArgumentException e) {
+            assertSame(IllegalArgumentException.class, e.getClass());
+            assertEquals("MyBeanAnother is abstract", e.getMessage());
+        }
     }
 
     @Test
     void should_nothing_happen_when_beanClazz_already_registred() {
+        IoCContext context = new IoCContextImpl();
         boolean happenSomething = false;
         try {
             context.registerBean(MyBean.class);
             context.registerBean(MyBean.class);
-        } catch( Exception e){
+        } catch (Exception e) {
             happenSomething = true;
         }
         assertFalse(happenSomething);
+    }
+
+    @Test
+    void should_throw_a_exception_when_start_call_getBean_mathod() {
+        IoCContext context = new IoCContextImpl();
+        context.registerBean(MyBean.class);
+        Stream<Integer> stream = Stream.iterate(1, i -> i).limit(10000);
+        try {
+            stream.parallel().map(item -> {
+                context.getBean(MyBean.class);
+                return item;
+            }).count();
+        } catch (IllegalStateException e) {
+            assertSame(IllegalStateException.class, e.getClass());
+        }
+
     }
 }
